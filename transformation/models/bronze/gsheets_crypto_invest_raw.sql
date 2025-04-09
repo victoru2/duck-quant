f6457f1563442095857e348ea124ba0ef0b94b88
vurquiola <victor.urquiola@muttdata.ai>
2025-04-09 17:50:17 -0300

    modeling new raw format

--- transformation/models/bronze/gsheets_crypto_invest_raw.sql
+++ transformation/models/bronze/gsheets_crypto_invest_raw.sql
@@ -3,41 +3,24 @@
     schema='bronze'
 ) }}
 
-WITH source_values AS (
-    SELECT
-        _dlt_parent_id AS transaction_id,
-        MAX(CASE WHEN data_values._dlt_list_idx = 0 THEN value END) AS date,
-        MAX(CASE WHEN data_values._dlt_list_idx = 1 THEN value END) AS coin,
-        MAX(CASE WHEN data_values._dlt_list_idx = 2 THEN value END) AS usd_price,
-        MAX(CASE WHEN data_values._dlt_list_idx = 3 THEN value END) AS amount,
-        MAX(CASE WHEN data_values._dlt_list_idx = 4 THEN value END) AS fee,
-        MAX(CASE WHEN data_values._dlt_list_idx = 5 THEN value END) AS fee_coin,
-        MAX(CASE WHEN data_values._dlt_list_idx = 6 THEN value END) AS tx_id,
-        MAX(CASE WHEN data_values._dlt_list_idx = 7 THEN value END) AS total_amount,
-        MAX(CASE WHEN data_values._dlt_list_idx = 8 THEN value END) AS fiat_amount,
-        MAX(CASE WHEN data_values._dlt_list_idx = 9 THEN value END) AS fiat_price,
-        MAX(CASE WHEN data_values._dlt_list_idx = 10 THEN value END) AS fiat_currency,
-        MAX(CASE WHEN data_values._dlt_list_idx = 11 THEN value END) AS type,
-        MAX(CASE WHEN data_values._dlt_list_idx = 12 THEN value END) AS exchange,
-        MAX(CASE WHEN data_values._dlt_list_idx = 14 THEN value END) AS notes
-    FROM 
-        {{ source('landing_zone', 'load_gsheets_data__value') }} data_values
-    GROUP BY 
-        _dlt_parent_id
-)
 SELECT
     date,
     coin,
-    usd_price,
-    amount,
+    "usd price" AS usd_price,
+    amount, 
     fee,
-    fee_coin,
-    tx_id,
-    type,
-    exchange,
+    "fee coin" AS fee_coin,
+    "tx id" AS tx_id,
+    "total amount" AS total_amount, 
+    "fiat amount" AS fiat_amount, 
+    "fiat price" AS fiat_price, 
+    "fiat currency" AS fiat_currency, 
+    "type" AS transaction_type, 
+    exchange, 
+    total, 
     notes
 FROM 
-    source_values
+    {{ source('landing_zone', 'crypto_invest') }}
 WHERE
     coin <> 'coin'
     AND coin IS NOT NULL
